2c1d5ad40a6a61df1b3f2f84ea68c8f601618bfc
ChrisSoussa <145501872+ChrisSoussa@users.noreply.github.com>
2024-03-08 09:42:40 -0500

    Update MainApp.java

--- src/main/java/Main/MainApp.java
+++ src/main/java/Main/MainApp.java
@@ -13,14 +13,7 @@ import org.slf4j.LoggerFactory;
 
 //hello
 
-/**
- * This is a JavaFX project template to be used for creating GUI applications.
- * The JavaFX GUI framework (version: 20.0.2) is linked to this project in the
- * build.gradle file.
- * @link: https://openjfx.io/javadoc/20/
- * @see: /Build Scripts/build.gradle
- * @author JAcob Davy.
- */
+
 public class MainApp extends Application {
 
     private final static Logger logger = LoggerFactory.getLogger(MainApp.class);
@@ -35,7 +28,7 @@ public class MainApp extends Application {
             loader.setController(new FXMLMainMenuController());
             Pane root = loader.load();
             //-- 2) Create and set the scene to the stage.
-            Scene scene = new Scene(root, 600, 300);
+            Scene scene = new Scene(root, 600, 400);
             primaryStage.setScene(scene);
             primaryStage.sizeToScene();
             // We just need to bring the main window to front.
